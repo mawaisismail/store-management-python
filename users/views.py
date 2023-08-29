925f9cd00103e8b53709705cba75d714c1590c48
mawaisismail <muhammad.awais.77831@gmail.com>
2023-08-29 13:27:42 +0500

    Added signup page and urls

--- users/views.py
+++ users/views.py
@@ -2,5 +2,9 @@ from django.http import HttpResponse
 from django.template.loader import get_template
 
 
-def login_page(response):
+def login(response):
     return HttpResponse(get_template("login.html").render())
+
+
+def signup(response):
+    return HttpResponse(get_template("signup.html").render())

7922f854d9f0561c8a680d7aca66b46e3fc210ce
mawaisismail <muhammad.awais.77831@gmail.com>
2023-08-29 12:41:58 +0500

    Added postgres

--- project/settings.py
+++ project/settings.py
@@ -73,8 +73,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '1122',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 

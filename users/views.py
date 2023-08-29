0a6ea005f4fe69a93d17f5728b662cfbcee94cf1
mawaisismail <muhammad.awais.77831@gmail.com>
2023-08-29 13:59:12 +0500

    fix signup/login form

--- users/views.py
+++ users/views.py
@@ -1,10 +1,14 @@
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
 from django.template.loader import get_template
+from django.db import models
 
 
-def login(response):
-    return HttpResponse(get_template("login.html").render())
+def login(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    if request.method == "POST":
+        return redirect("/")
 
 
-def signup(response):
-    return HttpResponse(get_template("signup.html").render())
+def signup(request):
+    return render(request, 'signup.html')

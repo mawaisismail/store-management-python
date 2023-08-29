78a6fbe43228acefcc49b54c2b7871dfb79c12e3
mawaisismail <muhammad.awais.77831@gmail.com>
2023-08-29 14:57:30 +0500

    added auth for User

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login),
+    path('login/', views.login_user),
     path('signup/', views.signup),
 ]
--- users/views.py
+++ users/views.py
@@ -1,14 +1,38 @@
-from django.shortcuts import render,redirect
-from django.template.loader import get_template
-from django.db import models
-
-
-def login(request):
-    if request.method == "GET":
-        return render(request, 'login.html')
-    if request.method == "POST":
-        return redirect("/")
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == "GET":
+        return render(request, 'signup.html')
+    if request.method == "POST":
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        confirm_password = request.POST.get("confirm_password")
+        if confirm_password != password:
+            return render(request, "signup.html")
+        user = User.objects.filter(username=email).exists()
+        if user:
+            return render(request, "signup.html")
+        new_user = User.objects.create_user(email=email, password=password, username=email, first_name=first_name,
+                                            last_name=last_name)
+        new_user.save()
+        print(new_user)
+        return redirect("/user/login")
+
+
+def login_user(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            print(user)
+            login(request, user)
+            return redirect('/')
+        return render(request, 'login.html')

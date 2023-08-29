961eaef4997926e9a55694e1c8f09ba99b13f349
mawaisismail <muhammad.awais.77831@gmail.com>
2023-08-29 19:00:22 +0500

    integrate protected routes

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from .forms import UserForgetPassword
 
 
@@ -44,6 +45,7 @@ def logout_user(request):
     return redirect('/')
 
 
+@login_required
 def change_password(request):
     form = UserForgetPassword()
     return render(request, "change-password.html", {"form": form})

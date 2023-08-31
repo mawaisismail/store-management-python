df2ca0d3f4dd5553b745fda3dc9d5bd038ea31c9
mawaisismail <muhammad.awais.77831@gmail.com>
2023-08-31 15:00:40 +0500

    added product models

--- project/settings.py
+++ project/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UsersConfig'
+    'users.apps.UsersConfig',
+    'products.apps.ProductsConfig'
 ]
 
 MIDDLEWARE = [

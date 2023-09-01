ba0ad042c81320da09bd3bfafd5b22a500d34e3d
mawaisismail <muhammad.awais.77831@gmail.com>
2023-09-01 16:58:09 +0500

    integrate get product & product by id and map data to template

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.products_page),
-    path('details/<id>', views.products_detail_page),
+    path('details/<product_id>', views.products_detail_page),
 ]
--- products/views.py
+++ products/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from .models import ProductsModel
-from django.db import transaction, DatabaseError
+from django.http import Http404
 
 
 def products_page(request):
     try:
         products = ProductsModel.objects.all()
-        print(products)
         return render(request, 'products.html', {"products": products})
-    except DatabaseError:
-        return render(request, 'products.html')
+    except Exception as e:
+        raise Http404("Something went wrong")
 
 
-def products_detail_page(request, id):
-    return render(request, 'product-detail.html')
+def products_detail_page(request, product_id):
+    try:
+        product = ProductsModel.objects.get(id=product_id)
+        return render(request, 'product-detail.html', {"product": product})
+    except Exception:
+        raise Http404("Something went wrong")
